eca71df50abe9680e1617ec56b830b3559d89f31
mpage544165 <55064915+mpage544165@users.noreply.github.com>
2020-05-05 21:22:00 -0700

    Refactor and comment

--- src/world_view_interpreter.cpp
+++ src/world_view_interpreter.cpp
@@ -1,3 +1,14 @@
+
+/*  @File: World_view_interpreter.cpp
+
+    @Brief: Generates a 3D map of the drone's environment using the LiDAR and Octomap Library
+
+    @Author: Matthew Page
+
+    @Date: 1/27/2020
+
+*/
+
 #include <math.h>
 
 #include "ros/ros.h"
@@ -12,19 +23,25 @@
 
 class WorldViewInterpreter{
 
+  // Declare pointclouds
   std::vector<geometry_msgs::Point32> pointCloud;
   sensor_msgs::PointCloud rosPointCloud;
 
-  ros::NodeHandle nodeHandle;
+  // Declare ROS node, publishers, and subscribers
+  ros::NodeHandle node;
   ros::Subscriber laserSub;
   ros::Subscriber lidarOrientationSub;
   ros::Subscriber navDataSub;
   ros::Publisher pointCloudPub;
   ros::Rate loop_rate = ros::Rate(30);
 
-  octomap::OcTree ocTree = octomap::OcTree(.1); // create ocTree with resolution .1
-  octomap::point3d sensorOrigin = octomap::point3d(0, 0, 0); // the origin of the sensor
-  
+  // create ocTree with resolution .1
+  octomap::OcTree ocTree = octomap::OcTree(.1);
+
+  // the origin of the sensor 
+  octomap::point3d sensorOrigin = octomap::point3d(0, 0, 0);
+
+  // Current oriantation and position of the lidar
   float orientation = 0;
   float x = 0;
   float y = 0;
@@ -41,29 +58,34 @@ class WorldViewInterpreter{
   WorldViewInterpreter(){
 
     // initialize laser scan subscriber
-    laserSub = nodeHandle.subscribe("/laser_publisher/laser_scan", 100, &WorldViewInterpreter::laserToPoint, this);
+    laserSub = node.subscribe("/laser_publisher/laser_scan", 100, &WorldViewInterpreter::laserToPoint, this);
 
     // initialize subsriber to lidar orientation
-    lidarOrientationSub = nodeHandle.subscribe("/laser_publisher/lidar_orientation", 100, &WorldViewInterpreter::onRotation, this);
+    lidarOrientationSub = node.subscribe("/laser_publisher/lidar_orientation", 100, &WorldViewInterpreter::onRotation, this);
 
-    navDataSub = nodeHandle.subscribe("/ground_truth/state", 100, &WorldViewInterpreter::onPosUpdate, this);
-	  
-    // publish point cloud
-    pointCloudPub = nodeHandle.advertise<sensor_msgs::PointCloud>("/point_cloud", 1);
-    minimumDistancePub = nodeHandle.advertise<std_msgs::Float32>("/minimum_distance", 100);
+    // subscribe to drone's navigation data
+    navDataSub = node.subscribe("/ground_truth/state", 100, &WorldViewInterpreter::onPosUpdate, this);
+
+    // create point cloud publisher
+    pointCloudPub = node.advertise<sensor_msgs::PointCloud>("/point_cloud", 1);
+    minimumDistancePub = node.advertise<std_msgs::Float32>("/minimum_distance", 100);
   }
 
-  // callback function for laser scan subscriber
+  /* @Brief: converts a lidar laser scan data to a set of 3D cartesian coordinates
+     @Param: msg - the ROS Laser Scan message produced from the Lidar
+     @Return: void
+  */ 
   void laserToPoint(const sensor_msgs::LaserScan::ConstPtr& msg){
-    
+
+    // check if the Lidar is enabled
     bool is_reading_LiDAR;
-    nodeHandle.getParam("/is_reading_LiDAR", is_reading_LiDAR);
+    node.getParam("/is_reading_LiDAR", is_reading_LiDAR);
 
     if(is_reading_LiDAR == false)
-	      return;
+	return;
 
+    // get number of lasers
     int numLasers = (msg->angle_max - msg->angle_min) / msg->angle_increment;
-    //std::cout << "numLasers: " << numLasers << std::endl;
     geometry_msgs::Point32 point;
 
     float distance;
@@ -75,81 +97,106 @@ class WorldViewInterpreter{
 
     // loop through each laser instance in the laser scan
     for(int i = 0; i < 64; i++){
-      // calculate parameters of laser 
+
+      // calculate parameters of each individual laser beam
       distance = msg->ranges[i];
       distanceSum += distance;
       horizontalAngle = this->orientation;
       verticalAngle= (1.5708 - (msg->angle_min + (i * msg->angle_increment)) + 0.785398);
 
-      // convert laser parameters to point
+      // convert laser parameters to a point
       point.x = getX(distance, horizontalAngle, verticalAngle);
       point.y = getY(distance, horizontalAngle, verticalAngle);
       point.z = -getZ(distance, verticalAngle);
 
-      this->pointCloud.push_back(point); // add point to point cloud
+      // add point to ROS and Octomap point cloud
+      this->pointCloud.push_back(point);
       
-      ocPointCloud.push_back(point.x, point.y, point.z); // add
+      ocPointCloud.push_back(point.x, point.y, point.z);
     }
 
     // written by courtney
     // set the minimum distance variable to the average ot the lidar point distances
     minDist.data = distanceSum / numLasers;
     minimumDistancePub.publish(minDist);
-    ros::Duration(0.5).sleep();
     loop_rate.sleep();
     minDist.data = 20;
     // end of code written by courtney
 
-    ocTree.insertPointCloud(ocPointCloud, this->sensorOrigin); // update ocTree
-    
+    // update ocTree with new pointcloud
+    ocTree.insertPointCloud(ocPointCloud, this->sensorOrigin); 
+
+    // if the save_octomap button is pressed, save the octomap to a file
     bool save_octomap;
-    nodeHandle.getParam("/save_octomap", save_octomap);
+    node.getParam("/save_octomap", save_octomap);
 
     if(save_octomap == true){
       ocTree.writeBinary("simple_tree.bt"); // save ocTree
-      nodeHandle.setParam("/save_octomap", false);
+      node.setParam("/save_octomap", false);
     }
     
-    //std::cout << "Done" << std::endl;
 
-    // populate ros point cloud
+    // populate ROS point cloud for visualization using rviz
     rosPointCloud.points.resize(pointCloud.size());
     std::copy(pointCloud.begin(), pointCloud.end(), std::back_inserter(rosPointCloud.points));
 
     rosPointCloud.header.frame_id = "my_frame";
 
-    pointCloudPub.publish(rosPointCloud); // publish ros point cloud
+    // publish ros point cloud
+    pointCloudPub.publish(rosPointCloud);
     loop_rate.sleep();
   }
 
-  // callback function for lidar rotation
+  /* @Brief: Retrives the current lidar orientation after the lidar rotates
+     @Param: msg - the published lidar orientation parameter
+     @Return: void
+  */
   void onRotation(const std_msgs::Float32ConstPtr& msg){
     this->orientation = msg->data;
   }
-	
+
+  /* @Brief: Retrives the current position of the drone
+     @Param: msg - the drone's published navigation data 
+     @Return: void
+  */
   void onPosUpdate(const nav_msgs::OdometryConstPtr& msg){
     this->x = msg->pose.pose.position.x;
     this->y = msg->pose.pose.position.y;
     this->z = msg->pose.pose.position.z;
   }
 
-  // get x coordinate
+  /* @Brief: Calculates the x coordinate of a laser beam endpoint
+     @Param: distance - the distance of the laser beam
+     @Param: horizontalAngle - the horizontal angle of the laser beam
+     @Param: verticalAngle - the vertical angle of the laser beam
+     @Return: the x coordinate of the laser beam endpoint
+  */
   float getX(float distance, float horizontalAngle, float verticalAngle){
     return distance * sin(verticalAngle) * cos(horizontalAngle) + this->x; 
   }
 
-  // get y coordinate
+  /* @Brief: Calculates the y coordinate of a laser beam endpoint
+     @Param: distance - the distance of the laser beam
+     @Param: horizontalAngle - the horizontal angle of the laser beam
+     @Param: verticalAngle - the vertical angle of the laser beam
+     @Return: the y coordinate of the laser beam endpoint
+  */
   float getY(float distance, float horizontalAngle, float verticalAngle){
     return distance * sin(verticalAngle) * sin(horizontalAngle) + this->y;
   }
 
-  // get z coordinate
+  /* @Brief: Calculates the z coordinate of a laser beam endpoint
+     @Param: distance - the distance of the laser beam
+     @Param: verticalAngle - the vertical angle of the laser beam
+     @Return: the z coordinate of the laser beam endpoint
+  */
   float getZ(float distance, float verticalAngle){
-    return distance * cos(verticalAngle) + this->z;
+    return -distance * cos(verticalAngle) + this->z;
   }
   
 };
 
+// main driver function
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "lidar_listener");
