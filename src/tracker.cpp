464a8ca55fe73361b3e4a4a45553cc8b0233593c
mpage544165 <55064915+mpage544165@users.noreply.github.com>
2020-05-05 21:17:56 -0700

    Refactor and comments

--- src/tracker.cpp
+++ src/tracker.cpp
@@ -1,3 +1,14 @@
+
+/*  @File: tracker.cpp
+
+    @Brief: This class implements an OpenCV Tracker to allow the drone to follow and track an object.
+
+    @Author: Matthew Page
+
+    @Date: 11/6/2019
+
+*/
+
 #include <ros/ros.h>
 #include <image_transport/image_transport.h>
 #include "geometry_msgs/Twist.h"
@@ -9,256 +20,182 @@
 #include <opencv2/tracking.hpp>
 #include <opencv2/features2d.hpp>
 
-static const std::string OPENCV_WINDOW = "Image window";
-
 static bool initialized = false;
-static bool calibrated = false;
 
-//known measurements in inches
-static int const KNOWN_DISTANCE = 157;
-static int const KNOWN_WIDTH = 39;
-
-class ImageConverter
+class Tracker
 {
-  ros::NodeHandle nh_;
-  image_transport::ImageTransport it_;
-  image_transport::Subscriber image_sub_;
-  image_transport::Publisher image_pub_;
-  ros::Publisher velPub_;
 
-  ros::Rate loop_rate = ros::Rate(30);
+    // Ros node handle, publishers, and subscribers
+    ros::NodeHandle node;
+    image_transport::ImageTransport it;
+    image_transport::Subscriber image_sub;
+    image_transport::Publisher image_pub;
+    ros::Publisher velPub;
 
-  cv::Rect2d roi;
-  cv::Ptr<cv::Tracker> tracker = cv::TrackerKCF::create();
-  cv::Ptr<cv::SimpleBlobDetector> detector = cv::SimpleBlobDetector::create();
+    // Ros publish rate
+    ros::Rate loop_rate = ros::Rate(30);
 
-  cv::RotatedRect rect;
-  int focalLength; 
+    // Initialize openCV tracker and blob detector
+    cv::Rect2d roi;
+    cv::Ptr<cv::Tracker> tracker = cv::TrackerKCF::create();
+    cv::Ptr<cv::SimpleBlobDetector> detector = cv::SimpleBlobDetector::create();
 
-public:
-  ImageConverter()
-    : it_(nh_)
-  {
-    // Subscribe to input video feed and publish output video feed
-    image_sub_ = it_.subscribe("/ardrone/front/image_raw", 1,
-      &ImageConverter::imageCb, this);
+    public:
 
-    image_pub_ = it_.advertise("/image_converter/output_video", 1);
-
-    velPub_ = nh_.advertise<geometry_msgs::Twist>("cmd_vel", 1);
-
-    cv::namedWindow(OPENCV_WINDOW);
-
-  }
-
-  ~ImageConverter()
-  {
-    cv::destroyWindow(OPENCV_WINDOW);
-  }
-
-  cv::RotatedRect findMarker(cv::Mat image){
-    cv::Mat gray;
-    cv::cvtColor(image, gray, CV_BGR2GRAY);
-    cv::GaussianBlur(gray, gray, cv::Size(5, 5), 0);
-    cv::Mat edges;
-    cv::Canny(gray, edges, 35, 125);
-
-    //cv::imshow("edges", edges);
-
-    std::vector<std::vector<cv::Point> > contours;
-    std::vector<cv::Vec4i> hierarchy;
-
-    cv::findContours(edges, contours, hierarchy, CV_RETR_CCOMP, CV_CHAIN_APPROX_SIMPLE );
-
-    int maxArea = cv::contourArea(contours[0]);
-    int index = 0;
-
-    for(int i = 1; i < contours.size(); i++){
-        if(cv::contourArea(contours[i]) > maxArea){
-            maxArea = cv::contourArea(contours[i]);
-            index = i;
-        }
-    }
-
-    return cv::minAreaRect(contours[index]);
-  }
-  
-  float findBlobs(cv::Mat image, cv::Rect2d roi){
-    // Read image
-    //Mat im = imread( "blob.jpg", IMREAD_GRAYSCALE );
-    cv::cvtColor(image, image, CV_BGR2GRAY);
-    
-    // Detect blobs.
-    std::vector<cv::KeyPoint> keypoints;
-    detector->detect(image, keypoints);
-
-    // Draw detected blobs as red circles.
-    // DrawMatchesFlags::DRAW_RICH_KEYPOINTS flag ensures the size of the circle corresponds to the size of blob
-    cv::Mat im_with_keypoints;
-    cv::drawKeypoints(image, keypoints, im_with_keypoints, cv::Scalar(0,0,255), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS );
-
-    // Show blobs
-    //cv::imshow("keypoints", im_with_keypoints );
-    //cv::waitKey(0);
-    float radius = 0;
-
-    for(int i = 0; i < keypoints.size(); i++){
-
-        if(sqrt(pow(keypoints[i].pt.x - (roi.x + ((float)roi.width / 2.0)), 2) 
-              + pow(keypoints[i].pt.y - (roi.y + ((float)roi.height / 2.0)), 2)) < 50){
-
-            radius = keypoints[i].size / 2.0;
-
-            cv::circle(im_with_keypoints, cv::Point(keypoints[i].pt.x, keypoints[i].pt.y), radius, cv::Scalar(0,255,0), 2.0); 
-        }
-    }
-
-    cv::imshow("blobs", im_with_keypoints);
-
-    return 3.1415 * (pow(radius / 2.0, 2)); //im_with_keypoints;
-  }
-
-  int distanceToCamera(int knownWidth, int focalLength, int perWidth){
-    try{
-        return (knownWidth * focalLength) / perWidth;
-    }
-    catch(int e){
-        return 0;
-    }
-  }
-
-  void updateOrientation(cv::Mat image, cv::Rect2d rect, float area){
-    int center_x = rect.x + (rect.width / 2);
-    int image_center_x = image.size().width / 2;
-
-    geometry_msgs::Twist msg;
-
-    if(center_x > image_center_x - 10 and center_x < image_center_x + 10){
-        msg.angular.z = 0.0;
-    }
-
-    else if(center_x > image_center_x){
-        msg.angular.z = -0.4;
-    }
-
-    else if(center_x < image_center_x){
-        msg.angular.z = 0.4;
-    }
-
-    if(area < 500){
-        msg.linear.x = 0.4;    
-    }
-    else{
-        msg.linear.x = 0;
-    }
-
-    velPub_.publish(msg);
-    loop_rate.sleep();
-  }
-
-  void updatePosition(int distance){
-
-    geometry_msgs::Twist msg;
-
-    if(distance < 50){
-        msg.linear.x = 1.0;    
-    }
-    else{
-        msg.linear.x = 0;
-        msg.linear.y = 0;
-        msg.linear.z = 0;
-    }
-
-    velPub_.publish(msg);
-    loop_rate.sleep();
-  }
-
-  void imageCb(const sensor_msgs::ImageConstPtr& msg)
-  {
-    cv_bridge::CvImagePtr cv_ptr;
-    try
+    Tracker()
+      : it(node)
     {
-      cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+        // Subscribe to input video feed and publish output video feed
+        image_sub = it.subscribe("/ardrone/front/image_raw", 1, &Tracker::imageCb, this);
+
+        image_pub = it.advertise("/tracker/output_video", 1);
+
+        velPub = node.advertise<geometry_msgs::Twist>("cmd_vel", 1);
+
     }
-    catch (cv_bridge::Exception& e)
+
+    /* @Brief: Finds and detects blobs in the drone's image feed
+       @Param: image - the image from the drone's camera
+       @Param: roi - the rectangular Region of Interest for tracking
+       @Return: the area of the tracked blob
+    */
+    float findBlobs(cv::Mat image, cv::Rect2d roi){
+        // convert to grayscale
+        cv::cvtColor(image, image, CV_BGR2GRAY);
+    
+        // Detect blobs using detector
+        std::vector<cv::KeyPoint> keypoints;
+        detector->detect(image, keypoints);
+
+        // Draw detected blobs as red circles.
+        cv::Mat im_with_keypoints;
+        cv::drawKeypoints(image, keypoints, im_with_keypoints, cv::Scalar(0,0,255), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS );
+
+        float radius = 0;
+
+        // iterate through each blob detected and sort by 
+        for(int i = 0; i < keypoints.size(); i++){
+
+            // if the center of a blob is close to the center of the selection rectangle, highlight it in green
+            if(sqrt(pow(keypoints[i].pt.x - (roi.x + ((float)roi.width / 2.0)), 2) 
+                  + pow(keypoints[i].pt.y - (roi.y + ((float)roi.height / 2.0)), 2)) < 50){
+
+                // save the radius of the blob
+                radius = keypoints[i].size / 2.0;
+
+                // Draw green circle around target object
+                cv::circle(im_with_keypoints, cv::Point(keypoints[i].pt.x, keypoints[i].pt.y), radius, cv::Scalar(0,255,0), 2.0); 
+            }
+        }
+
+        cv::imshow("blobs", im_with_keypoints);
+
+        //return the area of the blob closest to region of interest
+        return 3.1415 * (pow(radius / 2.0, 2));
+    }
+
+    /* @Brief: Updates the drone's position and orientation to keep the tracked object in the image frame
+       @Param: image - the image from the drone's camera
+       @Param: rect - the rectangular Region of Interest for tracking
+       @Param: area - the area of the blob being tracked
+       @Return: void
+    */
+    void updatePose(cv::Mat image, cv::Rect2d rect, float area){
+        int center_x = rect.x + (rect.width / 2);
+        int image_center_x = image.size().width / 2;
+
+        geometry_msgs::Twist msg;
+
+        // update orientation
+        if(center_x > image_center_x - 10 and center_x < image_center_x + 10){
+            msg.angular.z = 0.0;
+        }
+
+        else if(center_x > image_center_x){
+            msg.angular.z = -0.4;
+        }
+
+        else if(center_x < image_center_x){
+            msg.angular.z = 0.4;
+        }
+
+        // update position
+        if(area < 500){
+            msg.linear.x = 0.4;    
+        }
+        else{
+            msg.linear.x = 0;
+        }
+
+        // publish the generated velocity command
+        velPub.publish(msg);
+        loop_rate.sleep();
+    }
+
+    /* @Brief: Converts the ROS image message to an openCV image and performs tracking/position updates
+       @Param: msg - the ROS image from the drone's front camera
+       @Return: void
+    */
+    void imageCb(const sensor_msgs::ImageConstPtr& msg)
     {
-      ROS_ERROR("cv_bridge exception: %s", e.what());
-      return;
+        // convert the ROS image message to openCV image
+        cv_bridge::CvImagePtr cv_ptr;
+        try
+        {
+          cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+        }
+        catch (cv_bridge::Exception& e)
+        {
+          ROS_ERROR("cv_bridge exception: %s", e.what());
+          return;
+        }
+
+        // check if tracking is enabled
+        bool start_tracking;
+        node.getParam("/start_tracking", start_tracking);
+
+        if(start_tracking == false){
+            return;
+        }
+
+        // initialize tracker once tracking is enabled
+        if(initialized == false && start_tracking == true){
+            //cap >> frame;
+            roi = selectROI("tracker", cv_ptr->image);
+
+            tracker->init(cv_ptr->image, roi);
+
+            initialized = true;
+        }
+
+        // update the tracking result
+        tracker->update(cv_ptr->image, roi);
+
+        //perform blob detection
+        float area = findBlobs(cv_ptr->image, roi);
+
+        // draw tracked object
+        cv::rectangle(cv_ptr->image, roi, cv::Scalar( 255, 0, 0 ), 2, 1 );
+
+        // Check if autonomous mode is enabled before updating position
+        bool autonomous_movement;
+        node.getParam("/autonomous_movement", autonomous_movement);
+
+        if(autonomous_movement == true)
+	    updatePose(cv_ptr->image, roi, area);
+
+        cv::waitKey(3);
+
+        // Output modified video stream
+        image_pub.publish(cv_ptr->toImageMsg());
     }
-
-    bool start_tracking;
-    nh_.getParam("/start_tracking", start_tracking);
-
-    if(start_tracking == false){
-      return;
-    }
-
-    if(initialized == false && start_tracking == true){
-        //cap >> frame;
-        roi = selectROI("tracker", cv_ptr->image);
-
-        tracker->init(cv_ptr->image, roi);
-
-        initialized = true;
-    }
-
-    if(!calibrated){
-        //save reference image
-        //cv::imwrite("ref_image.png", cv_ptr->image);
-        
-
-        // calibrate camera
-        //cv::Mat im = cv::imread("ref_image.png");
-        //rect = findMarker(im);
-        //focalLength = (rect.size.width * KNOWN_DISTANCE) / KNOWN_WIDTH;
-        //std::cout << "focalLength: " << focalLength;
-
-        calibrated = true;
-    }
-
-    rect = findMarker(cv_ptr->image);
-    int inches = distanceToCamera(KNOWN_WIDTH, focalLength, rect.size.width);
- 
-    // display distance
-    std::stringstream ss;
-    ss << inches;
-    //putText(cv_ptr->image, ss.str() + " inches", cv::Point(20, 60), 
-    //           cv::FONT_HERSHEY_SIMPLEX, 2, cv::Scalar(255, 0, 0));
-
-    cv::rectangle(cv_ptr->image, rect.boundingRect(), cv::Scalar( 0, 255, 0 ), 2, 1 );
-
-    //updatePosition(inches);
-
-    //cv::imshow("image", cv_ptr->image);
-
-    // update the tracking result
-    tracker->update(cv_ptr->image, roi);
-    
-    float area = findBlobs(cv_ptr->image, roi);
-
-    // draw the tracked object
-    cv::rectangle(cv_ptr->image, roi, cv::Scalar( 255, 0, 0 ), 2, 1 );
-
-    updateOrientation(cv_ptr->image, roi, area);
-    
-    //cv::imshow("blobs", blobImage);
-
-    // show image with the tracked object
-    //imshow("tracker", cv_ptr->image);
-
-    // Update GUI Window
-    //cv::imshow(OPENCV_WINDOW, cv_ptr->image);
-    cv::waitKey(3);
-
-    // Output modified video stream
-    image_pub_.publish(cv_ptr->toImageMsg());
-  }
 };
 
+// main driver function
 int main(int argc, char** argv)
 {
-  ros::init(argc, argv, "image_converter");
-  ImageConverter ic;
+  ros::init(argc, argv, "tracker");
+  Tracker tracker;
   ros::spin();
   return 0;
 }
